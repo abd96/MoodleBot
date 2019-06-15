0c277754214372397134d65306b463ad64627b09
abd96 <boody634@gmail.com>
2019-06-15 20:00:24 +0200

    add a response handler

---
+++ Handlers/ResponseHandler.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+from Handlers.Handler import Handler
+
+
+class ResponseHandler(Handler):
+
+	
+	def __init__(self, response, session, loginScreen):
+		
+		self.response = response
+		self.session  = session
+		self.loginScreen = loginScreen
+
+	def isCourse(self, link):
+		return '/course' in link and "id=" in link
+
+	def getCourses(self):
+		
+		self.handling = True
+		links = []
+		# Parse the response text
+		soup = bs(self.response.text, 'html.parser')
+		tags = soup.findAll('a')
+		# extract all links that are course links from website
+		for tag in tags:
+			link = str(tag.get("href"))
+			if link not in links and self.isCourse(link) and not link == '#myoverview_courses_view': 
+					links.append(link)
+
+		self.handling = False
+		return links
